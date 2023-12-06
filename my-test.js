dbf92a3ecfe8c8b64f3360fe9e9e436d6889181d
Dhananjay Singh <42992869+dhananjaysr26@users.noreply.github.com>
2023-12-06 14:06:59 +0530

    Create my-test.js

---
+++ my-test.js
@@ -0,0 +1,21 @@
+(()=>{
+  console.log("Hello World IN !")
+document.addEventListener('DOMContentLoaded', function() {
+const body = document.querySelector('body');
+if (body) {
+  body.style.backgroundColor = 'red';
+} else {
+  console.log('Body element not found.');
+}
+    });
+setTimeout(function() {
+const body = document.querySelector('body');
+if (body) {
+  body.style.backgroundColor = 'red';
+} else {
+  console.log('Body element not found.');
+}
+}, 10000);
+})()
+  console.log("Hello World Out !")
+var test = 'aaaaaaaaa
